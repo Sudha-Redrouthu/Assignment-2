2da73b74b7d3a69f4259736ea61441663e2078f4
Sudha Rani Redroutu <Sredroutu2607@conestogac.on.ca>
2024-02-24 21:19:59 -0500

    Player Class Implementation

--- Assignment-2/Gem Hunters Game/Program.cs
+++ Assignment-2/Gem Hunters Game/Program.cs
@@ -23,5 +23,22 @@ public class Player
         Position = startPosition;
         GemCount = 0;
     }
+    public void Move(char direction)
+    {
+        switch (direction)
+        {
+            case 'U': Position = new Position(Position.X, Position.Y - 1); break;
+            case 'D': Position = new Position(Position.X, Position.Y + 1); break;
+            case 'L': Position = new Position(Position.X - 1, Position.Y); break;
+            case 'R': Position = new Position(Position.X + 1, Position.Y); break;
+        }
+    }
+
+    public void CollectGem()
+    {
+        GemCount++;
+    }
+}
+
 
 
