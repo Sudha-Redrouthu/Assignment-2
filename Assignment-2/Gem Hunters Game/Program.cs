7622a28f14568c5f33439d6e4a3205d31da7caf8
Sudha Rani Redroutu <Sredroutu2607@conestogac.on.ca>
2024-02-24 21:14:57 -0500

    Add project files.

---
+++ Assignment-2/Gem Hunters Game/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Position
+{
+    public int X { get; private set; }
+    public int Y { get; private set; }
+
+    public Position(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+}
